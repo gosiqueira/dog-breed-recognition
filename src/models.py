e9b5575b340d809d713a1471ecde2af365120885
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-06-07 13:13:11 -0300

    Update model to resnet50 backbone

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ from torchvision import models
 class DogBreedRecognizer(nn.Module):
     def __init__(self, n_classes):
         super(DogBreedRecognizer, self).__init__()
-        self.model = models.mobilenet_v3_small(pretrained=True)
+        self.model = models.resnet50(pretrained=True)
         self.model.fc = nn.Linear(2048, n_classes)
 
     def forward(self, x):
